eb450e4635848d64f6123ca06e78428f5c5e72a0
Liubo <you@example.com>
2023-09-16 10:06:39 +0300

    messige

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-//import { lazy, Suspense } from "react";//npm run build 
+import { lazy, Suspense } from "react"; 
 import { Routes, Route } from 'react-router-dom'; //npm i react-router-dom //npm start// for: path="/catalog"
 
 import { PostProvider } from './contexts/PostContext';
 import { AuthProvider } from './contexts/AuthContext';
 import PrivateRoute from "./components/common/PrivateRoute"; //not logged in user, not to see: create, edit...
-
- import  Header  from './components/Header/Header.js'
- import Home from './components/Home/Home'
- import  Register from './components/Register/Register.js'
- import  Login  from './components/Login/Login.js'
-import Logout from './components/Logout/Logout';
-import  Catalog  from './components/Catalog/Catalog'
-import  Details  from './components/Details/Details'
-import  Create  from './components/Create/Create'
-import  Edit  from './components/Edit/Edit'
-import MyPosts from './components/MyPosts/MyPosts'
-import Shopping from './components/Shopping/Shopping'
-import  Search  from './components/Search/Search'
-import  Footer  from './components/Footer/Footer'
-import  Page404  from './components/Page404/Page404'
-
 import './App.css';
 
-//const Search = lazy(() => import('./components/Search/Search'));
+ import  Header  from './components/Header/Header.js'
+ import  Footer  from './components/Footer/Footer'
+ const Home = lazy(() => import('./components/Home/Home'));
+ const Register = lazy(() => import('./components/Register/Register.js'));
+ const Login = lazy(() => import('./components/Login/Login.js'));
+const Logout = lazy(() => import('./components/Logout/Logout'));
+const Catalog = lazy(() => import('./components/Catalog/Catalog'));
+const Details = lazy(() => import('./components/Details/Details'));
+const Create = lazy(() => import('./components/Create/Create'));
+const Edit = lazy(() => import('./components/Edit/Edit'));
+const MyPosts = lazy(() => import('./components/MyPosts/MyPosts'));
+const Shopping = lazy(() => import('./components/Shopping/Shopping'));
+const Search = lazy(() => import('./components/Search/Search'));
+const Page404 = lazy(() => import('./components/Page404/Page404'));
+
 
 function App() {
   return (
@@ -32,15 +30,16 @@ function App() {
        <PostProvider>
        <Header />
 
-     <main className="main">                           
-       {/* <Suspense fallback>{<div>Loading...</div>} */}
+     <main className="main"> 
+           <Suspense fallback={<div>Loading...</div>}>          
      <Routes>
          <Route path="/" element={<Home />} /> 
          <Route path="/catalog" element={<Catalog />} />
        <Route path="/register" element={<Register />} />
        <Route path="/login" element={<Login />} />
        <Route path="/logout" element={<Logout />} />
-
+      
+     
        <Route path="/details/:id" element={(
             <PrivateRoute>
                <Details />
@@ -76,11 +75,14 @@ function App() {
                 <Search />
           </PrivateRoute>
       )} />
+        
 
     <Route path="*" element={<Page404 />} />
+  
     </Routes>
-    {/* </Suspense>  */}
+    </Suspense>
     </main>
+
     <Footer />
     </PostProvider> 
     </AuthProvider>
